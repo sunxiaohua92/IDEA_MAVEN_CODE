f1fa038d1d83fe37b633712576c20b0d6148f80c
“sunxiaohua92” <352393384@qq.com>
2021-06-07 16:31:00 +0800

    fist commit

---
+++ src/test/java/com/lemon/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.lemon;
+
+/**
+ * @Project: idea_test
+ * @Site: 天道酬勤！！
+ * @Forum: http://testingpai.com
+ * @Copyright: ©2021 版权所有 湖南省零檬信息技术有限公司
+ * @Author: Admin
+ * @Create: 2021-06-07 16:21
+ * @Desc：
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
